4ca5c27feb30eb0e355f268086488d3ae93d9c7f
vanyamelikov <vanyamelikov@yandex.ru>
2019-07-29 16:52:42 +0300

    Fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,17 @@ import { Nav } from './nav';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  menu:Nav[] = [
+  public menu:Nav[] = [
     {href: "#products", text:"Продукция"},
     {href: "#production", text:"Производство"},
     {href: "#about", text:"О компании"},
     {href: "#contacts", text:"Контакты"}
   ];
+
+  public advantages:boolean = false;
   scrollToElement($element): void {
     console.log($element);
     $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
-  
+
 }
